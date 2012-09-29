05ebf92c9e5bbef8fb1714255012777db9ac90bd
mistic100 <mistic@piwigo.org>
2012-09-29 10:00:12 +0000

    avoid false positive ("ass" won't block "classroom")
    git-svn-id: http://piwigo.org/svn/extensions/comments_blacklist@18370 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -12,11 +12,12 @@ function comm_blacklist_user_comment_check($comment_action, $comm)
   
   $blacklist = file(COMM_BLACKLIST_FILE, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
   $blacklist = array_map('strtolower', $blacklist);
-  $content = strtolower($comm['content']);
-  $author = strtolower($comm['author']);
+  $content = ' '.strtolower($comm['content']).' ';
+  $author = ' '.strtolower($comm['author']).' ';
   
   foreach ($blacklist as $word)
   {
+    $word = ' '.$word.' ';
     if ( strpos($content, $word)!==false or strpos($author, $word)!==false )
     {
       return $conf['comments_blacklist']['action'];
