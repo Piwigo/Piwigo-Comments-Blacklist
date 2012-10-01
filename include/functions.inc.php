de5ccc13067a8a37a0d7797f4ff88b5cee134378
mistic100 <mistic@piwigo.org>
2012-10-01 14:40:07 +0000

    use preg_match, usually faster
    git-svn-id: http://piwigo.org/svn/extensions/comments_blacklist@18394 68402e56-0260-453c-a942-63ccdbb3a9ee

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -5,23 +5,20 @@ function comm_blacklist_user_comment_check($comment_action, $comm)
 {
   global $conf;
   
-  if ($comment_action == $conf['comments_blacklist']['action'])
+  if ( $comment_action==$conf['comments_blacklist']['action'] or $comment_action=='reject' )
   {
     return $comment_action;
   }
   
   $blacklist = file(COMM_BLACKLIST_FILE, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-  $blacklist = array_map('strtolower', $blacklist);
-  $content = ' '.strtolower($comm['content']).' ';
-  $author = ' '.strtolower($comm['author']).' ';
+  $blacklist = array_map(create_function('$w', 'return " ".preg_quote($w)." ";'), $blacklist);
+  $blacklist = implode('|', $blacklist);
+
+  $content = str_replace(array("\r\n","\n"), ' ', $comm['content']);
   
-  foreach ($blacklist as $word)
+  if ( preg_match('#('.$blacklist.')#i', ' '.$comm['author'].' ') or preg_match('#('.$blacklist.')#i', ' '.$content.' ') )
   {
-    $word = ' '.$word.' ';
-    if ( strpos($content, $word)!==false or strpos($author, $word)!==false )
-    {
-      return $conf['comments_blacklist']['action'];
-    }
+    return $conf['comments_blacklist']['action'];
   }
   
   return $comment_action;
