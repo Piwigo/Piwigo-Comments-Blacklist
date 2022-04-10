8f8ad44aad7dbce66b1e26aa7affffc71b1e8b3d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-10 07:56:18 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Blacklist'] = 'Svartur listi';
+$lang['Moderate'] = 'Ritstýra';
+$lang['one word per line, case insensitive'] = 'eitt orð í hverri línu, ónæmt fyrir hástöfum';
