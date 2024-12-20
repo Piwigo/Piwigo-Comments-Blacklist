9310401621d5f83ed66c0e8ff01ab82599085748
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:06:23 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Blacklist'] = 'רשימת חסימה';
+$lang['Moderate'] = 'מידור';
+$lang['one word per line, case insensitive'] = 'מילה אחת לכל שורה (באנגלית, אין משמעות לאותיות גדולות/קטנות)';
