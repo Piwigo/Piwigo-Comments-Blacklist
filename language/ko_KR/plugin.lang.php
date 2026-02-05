e594f4b90784976ecb83ef375af59589b6ce0594
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-05 06:20:34 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Blacklist'] = '블랙리스트';
+$lang['Moderate'] = '보통';
+$lang['one word per line, case insensitive'] = '한 줄에 한 단어, 대소문자 구분 없음';
